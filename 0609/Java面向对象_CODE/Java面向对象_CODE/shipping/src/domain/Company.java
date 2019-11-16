35072cd1d73b88e4199fdeb51927fa413a622aae
zhanjie.jin <i@zhanjie.me>
2019-11-16 16:06:13 +0800

    添加Company类方法

--- 0609/Java面向对象_CODE/Java面向对象_CODE/shipping/src/domain/Company.java
+++ 0609/Java面向对象_CODE/Java面向对象_CODE/shipping/src/domain/Company.java
@@ -9,7 +9,35 @@ public class Company {
 	/**
 	 * @param args
 	 */
-	Set<Vehicle> vehicles;  //公司拥有0或者多种交通工具，“has关系”
+	private Set<Vehicle> vehicles;  //公司拥有0或者多种交通工具，“has关系”
+	private Set<Employee> employees;
+
+	public Company() {
+		super();
+	}
+
+	public Company(Set<Employee> employees, Set<Vehicle> vehicles) {
+		super();
+		this.employees = employees;
+		this.vehicles = vehicles;
+	}
+
+	public Set<Vehicle> getVehicles() {
+		return vehicles;
+	}
+
+	public void setVehicles(Set<Vehicle> vehicles) {
+		this.vehicles = vehicles;
+	}
+
+	public Set<Employee> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(Set<Employee> employees) {
+		this.employees = employees;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
